8e918883b401c631345afbb7b63965309f2b0e6f
samuagung-sys <samuagung@gmail.com>
2026-01-19 19:59:44 +0700

    feat/challenge done

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
-import App from './App';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
 
 const queryClient = new QueryClient();
 
